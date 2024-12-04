da074c77c9800246a5ba1958849d7851ced32229
Steve Hayes <Steve-Roderick@users.noreply.github.com>
2024-12-03 22:21:38 -0500

    day2 p2

--- day02/day2.go
+++ day02/day2.go
@@ -38,42 +38,58 @@ func parse_report(fpath string) [][]int {
 	return reports
 }
 
-func report_is_safe(reports [][]int) int {
-	safe := 0
-	for idx := range reports {
-		direction := 0
-		unsafe := false
-
-		for idy := range reports[idx] {
-			if idy == 0 {
-				continue
-			}
-			before := reports[idx][idy-1]
-			current := reports[idx][idy]
-			if direction == 0 {
-				if before > current {
-					direction = -1
-				} else if before < current {
-					direction = 1
-				}
-			}
-			diff := 0
-			if direction == 1 {
-				unsafe = current <= before
-				diff = current - before
-			} else if direction == -1 {
-				unsafe = current >= before
-				diff = before - current
-			} else {
-				unsafe = true
-			}
-			unsafe = unsafe || !(diff >= 1 && diff <= 3)
-			if unsafe {
-				break
+func level_is_safe(levels []int) bool {
+	direction := 0
+	unsafe := false
+	for idy := range levels {
+		if idy == 0 {
+			continue
+		}
+		before := levels[idy-1]
+		current := levels[idy]
+		if direction == 0 {
+			if before > current {
+				direction = -1
+			} else if before < current {
+				direction = 1
 			}
 		}
-		if !unsafe {
+		diff := 0
+		if direction == 1 {
+			unsafe = current <= before
+			diff = current - before
+		} else if direction == -1 {
+			unsafe = current >= before
+			diff = before - current
+		} else {
+			unsafe = true
+		}
+		unsafe = unsafe || !(diff >= 1 && diff <= 3)
+		if unsafe {
+			break
+		}
+	}
+	return !unsafe
+}
+
+func report_is_safe(reports [][]int, damper bool) int {
+	safe := 0
+	for idx := range reports {
+		ok := level_is_safe(reports[idx])
+		if ok {
 			safe += 1
+		} else if damper {
+			for index := range reports[idx] {
+				cpy := make([]int, 0)
+				cpy = append(cpy, reports[idx][:index]...)
+				cpy = append(cpy, reports[idx][index+1:]...)
+				ok := level_is_safe(cpy)
+				cpy = nil
+				if ok {
+					safe += 1
+					break
+				}
+			}
 		}
 	}
 	return safe
@@ -82,17 +98,26 @@ func report_is_safe(reports [][]int) int {
 func main() {
 
 	small_reports := parse_report("./small.txt")
-	safe1 := report_is_safe(small_reports)
+	safe1 := report_is_safe(small_reports, false)
 	fmt.Printf("Part I (small): %d\n", safe1)
 	if safe1 != 2 {
 		log.Fatal("Wrong Answer")
 	}
 
 	large_reports := parse_report("./large.txt")
-	safe2 := report_is_safe(large_reports)
+	safe2 := report_is_safe(large_reports, false)
 	fmt.Printf("Part I (large): %d\n", safe2)
 	if safe2 != 526 {
 		log.Fatal("Wrong Answer")
 	}
+	fmt.Printf("\n")
+
+	safe3 := report_is_safe(small_reports, true)
+	fmt.Printf("Part II (small): %d\n", safe3)
+	if safe3 != 4 {
+		log.Fatal("Wrong Answer")
+	}
+	safe4 := report_is_safe(large_reports, true)
+	fmt.Printf("Part II (large): %d\n", safe4)
 
 }
